ababd3e2dcb52f3006dde98abe13bf94242c7e06
younesga <39584305+younesgamer11@users.noreply.github.com>
2023-03-15 03:24:41 +0300

    Update dispatcher.py

--- chat_gpt_bot/dispatcher.py
+++ chat_gpt_bot/dispatcher.py
@@ -4,6 +4,6 @@ from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from chat_gpt_bot.config import BOT_TOKEN
 
 
-bot = Bot(token=BOT_TOKEN)
+bot = Bot(token=6247953312:AAFV_mYUJEzr3vd5qQtRfJCkQU6g54NxmhE)
 storage = MemoryStorage()
 dp = Dispatcher(bot, storage=storage)
